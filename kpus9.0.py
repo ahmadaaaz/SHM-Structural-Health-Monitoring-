9b3d31ba6ea6840b5e3fe762d58c5f72109ce9d7
ahmadaaaz <109924199+ahmadaaaz@users.noreply.github.com>
2026-05-12 12:16:02 +0300

    Update selectbox options and handling for damage index

--- kpus9.0.py
+++ kpus9.0.py
@@ -156,8 +156,10 @@ elif used_method == "3":
         di1, xi, yi, raw_d = get_damage_index(h1, d1, resolution, pre_smooth, epsilon_pct)
         di2, _, _, _ = get_damage_index(h2, d2, resolution, pre_smooth, epsilon_pct)
         di3, _, _, _ = get_damage_index(h3, d3, resolution, pre_smooth, epsilon_pct)
-        s_p = st.sidebar.selectbox("Seri/Paralel", ["seri", "paralel"])
-        if s_p is "paralel":
+        s_p = st.sidebar.selectbox("Seri/Paralel", ["normal", "seri", "paralel"])
+        if s_p is "normal":
+            di_total = ((di1/np.nanmax(di1)) + (di2/np.nanmax(di2)) + (di3/np.nanmax(di3)))
+        elif s_p is "paralel":
             di_total = (1/(di1/np.nanmax(di1)) + 1/(di2/np.nanmax(di2)) + 1/(di3/np.nanmax(di3)))**-1
         elif s_p is "seri":
             di_total = ((di1/np.nanmax(di1)) + (di2/np.nanmax(di2)) + (di3/np.nanmax(di3)))/ ((di1/np.nanmax(di1)) * (di2/np.nanmax(di2)) * (di3/np.nanmax(di3)))
