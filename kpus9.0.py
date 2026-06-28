8d6d78953c7457c3e93e654c57a414f6d0bf2217
ahmadaaaz <109924199+ahmadaaaz@users.noreply.github.com>
2026-06-28 15:24:13 +0300

    Remove extra whitespace in author caption

--- kpus9.0.py
+++ kpus9.0.py
@@ -232,7 +232,6 @@ with st.bottom:
     st.caption('''
     made by  \n
     Ahmed Ashraf Elsayed Zaghloul  &  MHD Yman Zain Al Abdien 
- 
      
     ahmed.zaghloul.ae@gmail.com  --  ymanzienalabdien@gmail.com
     ''')
