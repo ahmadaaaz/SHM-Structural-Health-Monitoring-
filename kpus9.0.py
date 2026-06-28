418250e9bf9a1a1438a3c80245050c5d6d776693
ahmadaaaz <109924199+ahmadaaaz@users.noreply.github.com>
2026-06-28 15:25:03 +0300

    Update kpus9.0.py

--- kpus9.0.py
+++ kpus9.0.py
@@ -231,7 +231,5 @@ with st.bottom:
     
     st.caption('''
     made by  \n
-    Ahmed Ashraf Elsayed Zaghloul  &  MHD Yman Zain Al Abdien 
-     
-    ahmed.zaghloul.ae@gmail.com  --  ymanzienalabdien@gmail.com
+    Ahmed Ashraf Elsayed Zaghloul  &  MHD Yman Zain Al Abdien‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ahmed.zaghloul.ae@gmail.com  --  ymanzienalabdien@gmail.com
     ''')
